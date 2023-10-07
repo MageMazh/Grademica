5b360f550f7f1b520bab05a68f3320c1536cca24
Andi Amjad <andiamjad@gmail.com>
2023-10-07 20:46:20 +0800

    Menambah List Profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonList, IonLabel } from '@ionic/react';
 import './Home.css';
 
 const Profile: React.FC = () => {
@@ -10,14 +9,79 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Profile</IonTitle>
+            <IonTitle size="large">test</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <IonList lines="full">
+          {/* 1. Profile Account */}
+          <IonItem><IonLabel>Account</IonLabel></IonItem>
+          
+          <IonItem>
+            <IonList lines="inset">
+              {/* list 1 */}
+              <IonItem><IonLabel>Nama :</IonLabel></IonItem>
+              {/* list 2 */}
+              <IonItem><IonLabel>NIP :</IonLabel></IonItem>
+              {/* list 3 */}
+              <IonItem><IonLabel>Email :</IonLabel></IonItem>
+
+            </IonList>
+          </IonItem>
+
+          {/* 2. Personal data */}
+          <IonItem><IonLabel>Data Pribadi</IonLabel></IonItem>
+
+          <IonItem>
+            <IonList>
+
+              {/* list 1 */}
+              <IonItem><IonLabel>Tempat & Tanggal Lahir :</IonLabel></IonItem>
+              {/* list 2 */}
+              <IonItem><IonLabel>Gander :</IonLabel></IonItem>
+              {/* list 3 */}
+              <IonItem><IonLabel>Agama :</IonLabel></IonItem>
+              {/* list 4 */}
+              <IonItem><IonLabel>WhatsApp :</IonLabel></IonItem>
+
+            </IonList>  
+          </IonItem>
+
+          {/* 3. Alamat Domisili */}
+          <IonItem><IonLabel>Alamat</IonLabel></IonItem>
+
+          <IonItem>
+            <IonList>
+
+              {/* list 1 */}
+              <IonItem><IonLabel>Alamat Domisili :</IonLabel></IonItem>
+              {/* list 2 */}
+              <IonItem><IonLabel>RT/RW :</IonLabel></IonItem>
+              {/* list 3 */}
+              <IonItem><IonLabel>Provinsi :</IonLabel></IonItem>
+              {/* list 4 */}
+              <IonItem><IonLabel>Kota/Kabupaten :</IonLabel></IonItem>
+              {/* list 5 */}
+              <IonItem><IonLabel>Kecamatan :</IonLabel></IonItem>
+              {/* list 6 */}
+              <IonItem><IonLabel>Kelurahaan/Desa :</IonLabel></IonItem>
+
+            </IonList>  
+          </IonItem>
+          
+
+
+        </IonList>
+
+
+        {/* <ExploreContainer /> */}
       </IonContent>
+      
     </IonPage>
   );
 };
