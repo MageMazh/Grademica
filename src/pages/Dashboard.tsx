27eb85a338a876f65f7a04b39fa22f19dfef47d2
[Hermanto Bandangan] <[hermantobandangan16@gmail.com]>
2023-10-10 12:53:33 +0800

    add menu and add css navbar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,13 @@
 import ExploreContainer from "../components/ExploreContainer";
+import Menu from "../components/menu";
 import Navbar from "../components/navbar";
 
 function Dashboard() {
   return (
+    <>
     <Navbar />
+    <Menu />   
+    </>
   );
 }
 
