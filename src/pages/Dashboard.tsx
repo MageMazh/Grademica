992e6e3164c29a39f0f66d7d5ea0936b529ee961
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-10-12 19:37:40 +0800

    added new feature in the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,83 @@
+import {
+  IonContent,
+  IonIcon,
+  IonSplitPane,
+  IonCard,
+  IonCardTitle,
+  IonCardContent,
+  IonGrid,
+  IonCol,
+  IonRow,
+} from "@ionic/react";
+import { bookOutline, personOutline, schoolOutline } from "ionicons/icons";
+
 import ExploreContainer from "../components/ExploreContainer";
 import Menu from "../components/menu";
 import Navbar from "../components/navbar";
+import "./Dashboard.css";
 
-function Dashboard() {
+const Dashboard: React.FC = () => {
+  const cardList = [
+    {
+      icon: schoolOutline,
+      description: "Jumlah SKS yang diampuh",
+      number: "14",
+    },
+    {
+      icon: personOutline,
+      description: "Jumlah mahasiswa yang diajar",
+      number: "190",
+    },
+    {
+      icon: bookOutline,
+      description: "Jumlah mata kuliah yang diambil",
+      number: "6",
+    },
+  ];
+
   return (
     <>
-    <Navbar />
-    <Menu />   
+      <Navbar />
+      <IonSplitPane className="split-pane" when="xs" contentId="main">
+        <Menu />
+        <div className="ion-page" id="main">
+          <IonContent className="dashboard ion-padding">
+            <h1>Dashboard</h1>
+            <h6>Hi, username</h6>
+            <IonGrid>
+              <IonRow>
+                {cardList.map((card, index) => (
+                  <IonCol>
+                    <IonCard className="dashboard-card">
+                      <div className="dashboard-card__background-icon">
+                        <IonIcon className="dashboard-card__icon" icon={card.icon}></IonIcon>
+                      </div>
+                      <IonCardContent className="dashboard-card__description">
+                        {card.description}
+                      </IonCardContent>
+                      <IonCardContent className="dashboard-card__number">
+                        {card.number}
+                      </IonCardContent>
+                    </IonCard>
+                  </IonCol>
+                ))}
+              </IonRow>
+            </IonGrid>
+
+            <IonCard className="dashboard-card">
+              <IonCardTitle className="dashboard-card__information">Informasi</IonCardTitle>
+              <IonCardContent>
+                <ol className="dashboard-card__list-information">
+                  <li>Dikarenakan sistem informasi ini masih dalam proses pengembangan maka kami memohon maaf jika terdapat kekurangan yang menyebabkan kekurangan nyaman anda. </li>
+                  <li>Diharapkan dapat menginput nilai seluruh mata kuliah sebelum batas penginputan nilai.</li>
+                </ol>
+              </IonCardContent>
+            </IonCard>
+          </IonContent>
+        </div>
+      </IonSplitPane>
     </>
   );
-}
+};
 
 export default Dashboard;
