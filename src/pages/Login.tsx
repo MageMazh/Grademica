9abcff5bc9805a91391ea56bbc958a7eb714fd57
[Sulkifli] <[skifli2795@gmail.com]>
2023-10-16 15:48:09 +0800

    add assets

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,28 @@
-import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonImg, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonImg, IonInput, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Login.css';
+import logounhas from '../assets/images/Logo_UH.webp'
 
 
 const Login: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent>      
-          <IonImg src="https://upload.wikimedia.org/wikipedia/id/9/95/Logo_UH.png" alt="Logounhas" ></IonImg>
-            <IonInput label='Username' label-placement='floating' fill='outline' placeholder='Enter text' className='ion-margin-top' ></IonInput>
-            <IonInput label='Password' label-placement='floating' fill='outline' placeholder='Enter text' type='password' className='ion-margin-top' ></IonInput>
-            <IonButton type='submit' expand='full' className='ion-margin-top' color={'primary'} >
-              Login
-            </IonButton>
+      <IonContent className='login_background'>
+        <IonGrid className='login-grid'>
+          <IonRow>
+            <IonCol>
+              <IonCard className='login_card'>
+                  <h1 className='text_1'>Grademica</h1>
+                  <IonImg src={logounhas}></IonImg>
+                  <IonInput className='login_input_username' label='Username' label-placement='floating' fill='solid' placeholder='Enter text' ></IonInput>
+                  <IonInput className='login_input_password' label='Password' label-placement='floating' fill='solid' placeholder='Enter text' type='password' ></IonInput>
+                  <IonButton className='login_button' type='submit' expand='full' color={'primary'} >
+                    Login
+                  </IonButton>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
