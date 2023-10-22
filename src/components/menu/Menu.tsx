a6d7b80c9841005016d4aa059883cfdece3dd7e4
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-10-23 00:08:39 +0800

    add new features

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,73 @@
 import {
   IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
   IonMenu,
+  IonMenuToggle,
+  IonTitle,
 } from "@ionic/react";
 import ExploreContainer from "../ExploreContainer";
 import "./Menu.css";
+import { homeOutline, helpCircleOutline, personOutline, libraryOutline, logOutOutline } from "ionicons/icons";
+import { useLocation } from "react-router";
+
+const appMenus = [
+  {
+    title: 'Dashboard',
+    url: '/dashboard',
+    iosIcon: homeOutline,
+    // mdIcon: homeSharp
+  },
+  {
+    title: 'Profile',
+    url: '/profile',
+    iosIcon: personOutline,
+    // mdIcon: paperPlaneSharp
+  },
+  {
+    title: 'Perkuliahan',
+    url: '/perkuliahan',
+    iosIcon: libraryOutline,
+    // mdIcon: heartSharp
+  },
+  {
+    title: 'About',
+    url: '/about',
+    iosIcon: helpCircleOutline,
+    // mdIcon: archiveSharp
+  },
+  {
+    title: 'Log-out',
+    url: '/login',
+    iosIcon: logOutOutline,
+    // mdIcon: trashSharp
+  },
+];
 
 const Menu: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <IonMenu className="split-pane__menu" contentId="main">
-      <IonContent className="ion-padding">MENU UTAMA</IonContent>
+    <IonMenu className="main-menu" contentId="main">
+      <IonContent className="main-menu__content">
+        <IonTitle className="ion-padding main-menu__title">
+        MENU UTAMA  
+        </IonTitle>
+      <IonList className="main-menu__list">
+        {appMenus.map((appMenu, index) =>  {
+          return(
+            <IonMenuToggle key={index} autoHide={false}>
+              <IonItem className={location.pathname === appMenu.url ? 'main-menu__list__selected' : 'main-menu__list__not-selected'} routerLink={appMenu.url}>
+                <IonIcon className="main-menu__list__icon" slot="start" ios={appMenu.iosIcon}/>
+                <IonLabel>{appMenu.title}</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+          );
+        })}
+      </IonList>
+        </IonContent>
     </IonMenu>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,35 +1,39 @@
 import {
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    IonItem,
-    IonAvatar,
-    IonLabel,
-  } from "@ionic/react";
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonAvatar,
+  IonLabel,
+  IonImg,
+} from "@ionic/react";
 
-import ExploreContainer from "../ExploreContainer";
+import "./Navbar.css"
+import logounhas from "../../assets/images/Logo_UH.webp";
 
 function Navbar() {
   return (
-    <IonHeader>
-    <IonToolbar>
-      <IonTitle>
-        Judul
-      </IonTitle>
-      <IonItem slot="end">
-        <IonLabel className="ion-margin-end">
-          username
-        </IonLabel>
-      <IonAvatar>
-          <img
-            alt="A person's head"
-            src="https://ionicframework.com/docs/img/demos/avatar.svg"
-          />
-        </IonAvatar>
-      </IonItem>
-    </IonToolbar>
+    <IonHeader className="header-navbar">
+      <div className="header-navbar__left">
+        <IonImg className="header-navbar__logo" src={logounhas}></IonImg>
+        <IonToolbar className="header-navbar__title">
+          <IonTitle>Grademica</IonTitle>
+          <IonTitle>UNIVERSITAS HASANUDDIN</IonTitle>
+        </IonToolbar>
+      </div>
+      <div>
+        <IonItem className="header-navbar__account">
+          <IonLabel>username</IonLabel>
+          <IonAvatar className="header-navbar__account__profile">
+            <img
+              alt="A person's head"
+              src="https://ionicframework.com/docs/img/demos/avatar.svg"
+            />
+          </IonAvatar>
+        </IonItem>
+      </div>
     </IonHeader>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
