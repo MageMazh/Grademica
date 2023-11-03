e3bff8b5aeea19bfa06e2738dbb45c6b195a7a05
[Sulkifli] <[skifli2795@gmail.com]>
2023-11-04 00:40:31 +0800

    update page ListCourse

--- src/components/views/ListCourseViews/ListCourseViews.tsx
+++ src/components/views/ListCourseViews/ListCourseViews.tsx
@@ -1,79 +1,206 @@
-import { 
-    IonContent,  
-    IonItem, 
-    IonList, 
-    IonLabel, 
-    IonAvatar,
-    IonCardHeader,
-    IonButton,
-    IonIcon,
-    IonSplitPane,
-    IonCard,
-    IonCardTitle,
-    IonCardContent,
-    IonGrid,
-    IonCol,
-    IonRow,
-    IonButtons,
-    IonRouterLink,
-   } from '@ionic/react';
+import {
+  MRT_GlobalFilterTextField,
+  MRT_TableBodyCellValue,
+  MRT_TablePagination,
+  MRT_ToolbarAlertBanner,
+  flexRender,
+  type MRT_ColumnDef,
+  useMaterialReactTable,
+} from 'material-react-table';
+import {
+  Box,
+  IconButton,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import {
+  IonContent,
+  IonItem,
+  IonList,
+  IonLabel,
+  IonAvatar,
+  IonCardHeader,
+  IonButton,
+  IonIcon,
+  IonSplitPane,
+  IonCard,
+  IonCardTitle,
+  IonCardContent,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonButtons,
+  IonRouterLink,
+} from '@ionic/react';
+
 import Navbar from '../../navbar';
 import Menu from '../../menu';
 import "./ListCourseViews.css"
 import { IonSearchbar } from '@ionic/react';
-import { courseData } from '../../../mockData/CourseData';
+import { course, data } from '../../../mockData/CourseData';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { peopleOutline } from "ionicons/icons";
+import { Link } from 'react-router-dom';
 
-  
-  
-  const ListCourseViews: React.FC = () => {
 
+const columns: MRT_ColumnDef<course>[] = [
+  {
+    accessorKey: 'no',
+    header: 'No',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Nama Matakuliah',
+  },
+  {
+    accessorKey: 'code',
+    header: 'Kode',
+  },
+  {
+    accessorKey: 'level',
+    header: 'Jenjang',
+  },
+  {
+    accessorKey: 'sks',
+    header: 'SKS',
+  },
+  {
+    accessorKey: 'semester',
+    header: 'Semester',
+  },
+  {
+    accessorKey: 'action',
+    header: 'Action',
+    // Definisikan sel untuk kolom Action dengan menggunakan flexRender
+    Cell: ({ row }) => {
       return (
-        <>
-        <Navbar />
-        <IonSplitPane className="split-pane" when="xs" contentId="main">
-          <Menu />
-          <div className="ion-page" id="main">
-            <IonContent className="dashboard ion-padding">
-                <h1>List Matakuliah</h1>
-                <IonCard className='card-list-matakuliah'>
-                  <div className='search-bar-listmatkul'>
-                    <IonButton className="button-add-matkul" routerLink="/perkuliahan/add-course">Add Mata Kuliah</IonButton>
-                    <IonSearchbar className='search-bar-list-matkul' ></IonSearchbar>
-                  </div>
-                    <IonGrid>
-                        <IonRow className="border-bottom">
-                        <IonCol className='col-1' size="0.7"><IonLabel>No</IonLabel></IonCol>
-                        <IonCol className='col-1' size="3.3"><IonLabel>Mata Kuliah</IonLabel></IonCol>
-                        <IonCol className='col-1' size="1.5"><IonLabel>Kode</IonLabel></IonCol>
-                        <IonCol className='col-1' size="1.5"><IonLabel>Jenjang</IonLabel></IonCol>
-                        <IonCol className='col-1' size="1.5"><IonLabel>SKS</IonLabel></IonCol>
-                        <IonCol className='col-1' size="1.5"><IonLabel>Semester</IonLabel></IonCol>
-                        <IonCol className='col-1' size="2"><IonLabel>Action</IonLabel></IonCol>
-                        </IonRow>
-                        {courseData.map((item) => (
-                        <IonRow key={item.no}>
-                            <IonCol className='col-1' size="0.7"><IonLabel>{item.no}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="3.3"><IonLabel>{item.name}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="1.5"><IonLabel>{item.code}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="1.5"><IonLabel>{item.level}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="1.5"><IonLabel>{item.sks}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="1.5"><IonLabel>{item.semester}</IonLabel></IonCol>
-                            <IonCol className='col-1' size="2">
-                            <IonButtons>
-                            <IonButton color={'primary'} routerLink='/perkuliahan/edit-course'>edit</IonButton>
-                            <IonButton color={'primary'}>detail</IonButton>
-                            </IonButtons>
-                            </IonCol>
-                        </IonRow>
-                        ))}
-                    </IonGrid>
-                    </IonCard>
-            </IonContent>
-          </div>
-        </IonSplitPane>                                 
-      </>
-    );
+        <div>
+          <IconButton component={Link} to="/perkuliahan/edit-course"
+            color="primary"
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            color="error"
+          >
+            <DeleteIcon />
+          </IconButton>
+          <IconButton component={Link} to="/perkuliahan/list-mahasiswa"
+          >
+            <IonIcon icon={peopleOutline}></IonIcon>
+          </IconButton>
+        </div>
+      );
+    },
+  },
+];
+
+const ListCourseViews = () => {
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enableRowSelection: false,
+    initialState: {
+      pagination: { pageSize: 20, pageIndex: 0 },
+      showGlobalFilter: true,
+    },
+    //customize the MRT components
+    muiPaginationProps: {
+      rowsPerPageOptions: [10, 15, 20, 25, 30, 35],
+      variant: 'outlined',
+    },
+    paginationDisplayMode: 'pages',
+  });
+
+  return (
+    <>
+    <Navbar />
+      <IonSplitPane className="split-pane" when="xs" contentId="main">
+        <Menu />
+        <div className="ion-page" id="main">
+          <IonContent className="dashboard ion-padding">
+            <h1>List Matakuliah</h1>
+            <IonCard className='card-list-matakuliah'>
+              <Stack>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}
+                >
+                  {/**
+                   * Use MRT components along side your own markup.
+                   * They just need the `table` instance passed as a prop to work!
+                   */}
+                  <IonButton routerLink="/perkuliahan/add-course">Add matakuliah</IonButton>
+                  <MRT_GlobalFilterTextField table={table} />
+                </Box>
+                {/* Using Vanilla Material-UI Table components here */}
+                <TableContainer className='border-list-mahasiswa'>
+                  <Table>
+                    {/* Use your own markup, customize however you want using the power of TanStack Table */}
+                    <TableHead className='border-botton'>
+                      {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                          {headerGroup.headers.map((header) => (
+                            <TableCell align="center" variant="head" key={header.id}>
+                              {header.isPlaceholder
+                                ? null
+                                : (
+                                  <span style={{
+                                    fontWeight: 'bold',
+                                    fontSize: "15px",
+                                  }}>
+                                    {flexRender(
+                                      header.column.columnDef.Header ??
+                                      header.column.columnDef.header,
+                                      header.getContext(),
+                                    )}
+                                  </span>
+                                )}
+                            </TableCell>
+                          ))}
+                        </TableRow>
+                      ))}
+                    </TableHead>
+                    <TableBody
+                      sx={{
+                        //stripe the rows, make odd rows a darker color
+                        '& tr:nth-of-type(even) > td': {
+                          backgroundColor: '#f5f5f5',
+                        },
+                      }}>
+                      {table.getRowModel().rows.map((row) => (
+                        <TableRow key={row.id} selected={row.getIsSelected()}>
+                          {row.getVisibleCells().map((cell) => (
+                            <TableCell align="center" variant="body" key={cell.id}>
+                              {/* Use MRT's cell renderer that provides better logic than flexRender */}
+                              <MRT_TableBodyCellValue cell={cell} table={table} />
+                            </TableCell>
+                          ))}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <MRT_TablePagination table={table} />
+                <MRT_ToolbarAlertBanner stackAlertBanner table={table} />
+              </Stack>
+            </IonCard>
+          </IonContent>
+        </div>
+      </IonSplitPane>
+    </>
+  );
 };
-  
+
 export default ListCourseViews;
-  
