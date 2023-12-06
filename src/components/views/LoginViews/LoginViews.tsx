3284202a31f39b8faf912523668ad44e5b6488f9
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2023-12-06 23:53:42 +0800

    implement firebase auth

--- src/components/views/LoginViews/LoginViews.tsx
+++ src/components/views/LoginViews/LoginViews.tsx
@@ -1,4 +1,5 @@
 import {
+  IonAlert,
   IonButton,
   IonCard,
   IonCol,
@@ -6,14 +7,52 @@ import {
   IonGrid,
   IonImg,
   IonInput,
-  IonPage,
   IonRow,
 } from "@ionic/react";
 import "./LoginViews.css";
 import logounhas from "../../../assets/images/Logo_UH.webp";
+import { useHistory } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebase/firebase";
+import { useState } from "react";
+import Cookies from 'js-cookie';
 
 const LoginViews: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertWrong, setAlertWrong] = useState(false);
+  const history = useHistory();
+
+  const signIn = async () => {
+    try {
+      // Perform the login
+      await signInWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      if (user) {
+        Cookies.set('authToken', user.uid);
+        history.push("/dashboard");
+      }
+
+    } catch (error) {
+      // Handle login failure
+      setAlertWrong(true);
+      console.error("Error signing in:", error);
+    }
+  };
+
   return (
+    <>
+      <IonAlert
+        isOpen={alertWrong}
+        trigger="present-alert"
+        header="Username dan password yang dimasukkan kurang tepat"
+        buttons={[
+          {
+            text: "OK",
+          },
+        ]}
+        onDidDismiss={() => setAlertWrong(false)}
+      ></IonAlert>
       <IonContent className="login_background">
         <IonGrid className="login-grid">
           <IonRow>
@@ -27,6 +66,11 @@ const LoginViews: React.FC = () => {
                   label-placement="floating"
                   fill="solid"
                   placeholder="Enter text"
+                  onIonInput={(e: Event) => {
+                    const value = (e.target as HTMLInputElement).value;
+                    console.log(value);
+                    setEmail(value);
+                  }}
                 ></IonInput>
                 <IonInput
                   className="login_input_password"
@@ -35,12 +79,18 @@ const LoginViews: React.FC = () => {
                   fill="solid"
                   placeholder="Enter text"
                   type="password"
+                  onIonInput={(e: Event) => {
+                    const value = (e.target as HTMLInputElement).value;
+                    console.log(value);
+                    setPassword(value);
+                  }}
                 ></IonInput>
                 <IonButton
                   className="login_button"
                   type="submit"
                   expand="full"
                   color={"primary"}
+                  onClick={signIn}
                 >
                   Login
                 </IonButton>
@@ -49,6 +99,7 @@ const LoginViews: React.FC = () => {
           </IonRow>
         </IonGrid>
       </IonContent>
+    </>
   );
 };
 
